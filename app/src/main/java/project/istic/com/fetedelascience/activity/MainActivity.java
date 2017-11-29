00b4822563772b3cab38a4e049c0d7e4b122a953
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-29 11:43:15 +0100

    Start add splashscreen

--- app/src/main/java/project/istic/com/fetedelascience/activity/MainActivity.java
+++ app/src/main/java/project/istic/com/fetedelascience/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package project.istic.com.fetedelascience;
+package project.istic.com.fetedelascience.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import project.istic.com.fetedelascience.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/project/istic/com/fetedelascience/activity/SplashActivity.java
@@ -0,0 +1,23 @@
+package project.istic.com.fetedelascience.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
