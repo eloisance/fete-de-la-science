47af7fa20b8ba875a3775072af0b14068da29509
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2018-02-15 22:31:16 +0100

    Add Projet PDF

--- app/src/main/java/project/istic/com/fetedelascience/activity/MainActivity.java
+++ app/src/main/java/project/istic/com/fetedelascience/activity/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Mandatory
         ButterKnife.bind(this);
 
         /*FirebaseDatabase database = FirebaseDatabase.getInstance();
